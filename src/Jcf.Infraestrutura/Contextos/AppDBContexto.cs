29c40e6aba49677f91f3c2f021688b0ab1973b80
João Carias de França <joaocariasdefranca@gmail.com>
2021-12-23 14:01:17 -0300

    Estratutura Básica

---
+++ src/Jcf.Infraestrutura/Contextos/AppDBContexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jcf.Infraestrutura.Contextos
+{
+    public class AppDBContexto : IdentityDbContext
+    {
+        public AppDBContexto(DbContextOptions<AppDBContexto> options) : base(options) { }
+    }
+}
